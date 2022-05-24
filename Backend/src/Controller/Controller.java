0258eae15b4348590b16f7e947e3d8944798a633
gal levy <gallevy34@cs.colman.ac.il>
2022-05-24 17:27:45 +0300

    new Commands
    add Command interface and Commands class

--- Backend/src/Controller/Controller.java
+++ Backend/src/Controller/Controller.java
@@ -1,4 +1,16 @@
 package Controller;
 
+<<<<<<< Updated upstream
+=======
+import Model.Model;
+
+import java.util.concurrent.BlockingQueue;
+
+//
+>>>>>>> Stashed changes
 public class Controller {
+
+    Model model;
+    BlockingQueue<Command> CommandsBQ;
+    
 }
