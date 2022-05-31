dc3c29245b6d24a39c41cb4ef20ba1e7e11c66f4
Guy Barzily <85988766+GuyBarzily@users.noreply.github.com>
2022-05-31 15:51:51 +0300

    connection to db init

--- Backend/src/main/java/Model/DataBase.java
+++ Backend/src/main/java/Model/DataBase.java
@@ -1,4 +1,37 @@
 package Model;
 
+import com.mongodb.client.*;
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class DataBase {
+    MongoClient client;
+    MongoDatabase database;
+    MongoIterable<String> list;
+    public DataBase(String connectionURL, String dbName ){
+       this.client = MongoClients.create(connectionURL);
+       this.database = this.client.getDatabase(dbName);
+       this.list = this.database.listCollectionNames();
+    }
+    public void createCollection(String colName){
+        this.database.createCollection(colName);
+//        Document sampleDoc = new Document("id", 1).append("name", "Yossi Smith");
+//        col.insertOne(sampleDoc);
+
+    }
+    public List<String> getCollections(){
+        List<String> connectionList = new ArrayList<>();
+        this.list.forEach(connectionList::add);
+        return connectionList;
+    }
+    public void addDocument(String colName, Document doc){
+        this.database.getCollection(colName).insertOne(doc);
+    }
+
+    public void closeClient(){
+        this.client.close();
+    }
 }
