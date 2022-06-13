bd1c591d7a78dd57774d60c7bc56a40a10f8a064
ofir6283 <ofir6283@gmail.com>
2022-06-13 21:44:44 +0300

    function signature
    updated function signature without plus/minus function

--- Frontend/src/main/java/com/example/frontend/windowController/FleetOverviewController.java
+++ Frontend/src/main/java/com/example/frontend/windowController/FleetOverviewController.java
@@ -6,6 +6,9 @@ import javafx.scene.chart.NumberAxis;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class FleetOverviewController {
 
     @FXML
@@ -16,32 +19,53 @@ public class FleetOverviewController {
     private NumberAxis y;
 
 
-    //Functions
+    //Features:
 
-    //doubleClick
+    // redirecting to "Monitoring" tab for additional information about specific airplane
     public void doubleClick() {
     }
 
-    //refreshMap
+    // refreshing all airplanes locations presented in the map
     public void refreshMap() {
     }
 
+    // manual refresh button of all the graphs
+    public void refreshButton() {
 
-    //singlePlaneInfo
-    public void singlePlaneInfo() {
+    }
+
+    // presenting a single plane information by clicking it once
+    public void singlePlaneInfo(String name, int direction, int altitude, int velocity) {
         // name of the plane
-        // flight direction
+        // flight direction (in degrees)
         // altitude (kilo feet - kft)
         // speed (knots - kn)
     }
 
 
-    //zoom?
+    //zoom in / out?
 
 
-    //direction
-    public void direction(){
-
+    // changing and presenting plane icon direction towards its location
+    public void direction(double longitude, double latitude) {
     }
 
+
+    //Charts:
+
+    // active planes compared to inactive planes
+    public void activePlanes(int avg) {
+    }
+
+    // sorted accumulated nautical miles for individual plane since the beginning of the month
+    public void singleSortedMiles(HashMap<Integer, List<Integer>> airplaneList) {
+    }
+
+    // presents average sorted nautical miles of all the fleet for every month since the beginning of the year
+    public void multipleSortedMiles(HashMap<Integer, List<Integer>> airplaneList) {
+    }
+
+    // presents the fleet size relative to time
+    public void lineChart(HashMap<Integer, List<Integer>> airplaneList) {
+    }
 }
