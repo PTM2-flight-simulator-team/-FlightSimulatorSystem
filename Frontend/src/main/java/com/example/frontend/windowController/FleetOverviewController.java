c21a58508e23d41d1c8b4f09604134a931d53af0
ofir6283 <ofir6283@gmail.com>
2022-06-13 17:11:22 +0300

    changes

--- Frontend/src/main/java/com/example/frontend/windowController/FleetOverviewController.java
+++ Frontend/src/main/java/com/example/frontend/windowController/FleetOverviewController.java
@@ -1,4 +1,47 @@
 package com.example.frontend.windowController;
 
+import javafx.fxml.FXML;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
 public class FleetOverviewController {
+
+    @FXML
+    private ImageView refreshBtn;
+    @FXML
+    private CategoryAxis x;
+    @FXML
+    private NumberAxis y;
+
+
+    //Functions
+
+    //doubleClick
+    public void doubleClick() {
+    }
+
+    //refreshMap
+    public void refreshMap() {
+    }
+
+
+    //singlePlaneInfo
+    public void singlePlaneInfo() {
+        // name of the plane
+        // flight direction
+        // altitude (kilo feet - kft)
+        // speed (knots - kn)
+    }
+
+
+    //zoom?
+
+
+    //direction
+    public void direction(){
+
+    }
+
 }
