c3eee9b9bb864b1470c133c19d80faad21a0bf98
roeyrach <93585090+roeyrach@users.noreply.github.com>
2022-06-13 10:17:32 +0300

    13.6 report
    big line chart and timeseries

--- Frontend/src/main/java/Model/Model.java
+++ Frontend/src/main/java/Model/Model.java
@@ -1,12 +1,11 @@
-package com.example.frontend;
+package Model;
 
 import java.io.*;
 import java.net.Socket;
-import java.util.HashMap;
-import java.util.Observable;
-import java.util.Observer;
+import java.util.*;
 
 public class Model extends Observable implements Observer {
+    private List<Double> joyStickData = new ArrayList<>();
     public Model(){
         new Thread("New Thread") {
             public void run(){
@@ -18,12 +17,19 @@ public class Model extends Observable implements Observer {
                 update(null,null);
             }
         }.start();
-        update(this,null);
+        //update(this,null);
     }
+
+    public void setJoyStickData(double d1, double d2) {
+        this.joyStickData.add(d1);
+        this.joyStickData.add(d2);
+    }
+
 
     @Override
     public void update(Observable o, Object arg) {
+        setJoyStickData(120,120);
         setChanged();
-        notifyObservers(200);
+        notifyObservers(joyStickData);
     }
 }
