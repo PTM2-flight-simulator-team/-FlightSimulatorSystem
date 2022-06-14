55b38e2f6c412994687c599c7c6f38ecb8bca3a5
gal levy <gallevy34@cs.colman.ac.il>
2022-06-14 17:04:48 +0300

    dont know
    what to write

--- Backend/src/main/java/Controller/ServerConnection/AgentConnections/AgentListener.java
+++ Backend/src/main/java/Controller/ServerConnection/AgentConnections/AgentListener.java
@@ -1,17 +1,21 @@
 package Controller.ServerConnection.AgentConnections;
 
+import CommonClasses.AnalyticsData;
 import CommonClasses.PlaneData;
 import Controller.Controller;
 
 import java.io.*;
 import java.net.Socket;
 import java.net.SocketException;
+import java.time.Month;
+import java.util.List;
 
 public class AgentListener implements Runnable {
     private Socket client;
     private ObjectInputStream in;
     private boolean running;
-    private PlaneData plainData;
+    private PlaneData planeData;
+    private List<List<String>> tsList;
 
     public AgentListener(Socket client) {
         this.client = client;
@@ -36,12 +40,37 @@ public class AgentListener implements Runnable {
                 Object fromAgent = in.readObject();// plaindata
 
                 if (fromAgent instanceof PlaneData) {
-                    plainData = (PlaneData)fromAgent;
-                    Controller.planeDataMap.put(plainData.getId(),plainData);
-                    plainData.Print();
+                    planeData = (PlaneData)fromAgent;
+                    Controller.planeDataMap.put(planeData.getId(),planeData);
+                    planeData.Print();
+                }
+                else if(fromAgent instanceof AnalyticsData){
+                    AnalyticsData tempAnalytics = (AnalyticsData)fromAgent;
+                    int month;
+                    Month strMonth;
+                    String[] date = tempAnalytics.getEndTime().split("-");//example--> 14-06-2022
+                    if(date[1].startsWith("0")){
+                        month = date[1].charAt(1);
+                        strMonth = Month.of(month);
+                    }
+                    else{
+                        month = date[1].charAt(0);
+                        strMonth = Month.of(month);
+                    }
+                    String tempPlaneId = this.planeData.getId();
+                    if(Controller.model.DB.doesPlaneExists(tempPlaneId)){//check if plane exists
+                        Controller.model.DB.updateMilesById(tempPlaneId , Double.valueOf(tempAnalytics.getMiles()), strMonth);
+                        Controller.model.DB.changePlaneState(tempPlaneId , tempAnalytics.getState());
+                    }
+                    else {
+
+                        Controller.model.DB.saveNewPlaneAnalytics(this.planeData.getId()
+                                ,this.planeData.getPlaneName(), strMonth ,  Double.valueOf(tempAnalytics.getMiles()) ,tempAnalytics.getState() );
+                    }
                 }
                 else{
-                    //t.s
+                    tsList = (List<List<String>>)fromAgent;
+                    Controller.model.DB.savePlaneTimeSeries(planeData.getId() ,planeData.getPlaneName() ,tsList);
                 }
             }catch (SocketException se){
                 this.stopListening();
@@ -52,7 +81,7 @@ public class AgentListener implements Runnable {
     }
 
     public void stopListening() {
-        Controller.planeDataMap.remove(this.plainData.getId());
+        Controller.planeDataMap.remove(this.planeData.getId());
         this.running = false;
         try {
             in.close();
